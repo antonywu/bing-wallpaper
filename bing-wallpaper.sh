4aa5a5810de0e82e3a370f5eb05af40ac1d85377
Antony Wu <antony@meshway.com>
2013-04-20 10:30:56 -0500

    Fix: script to support downloading files with unicode in filename
    Update script to remove all unicode, so that we can download and save files

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -10,10 +10,10 @@ curl "http://themeserver.microsoft.com/default.aspx?p=Windows&c=Flora&m=en-US" |
 #read file line
 while read line
 do
-    fileName=$(echo $line | sed -e "s;.*/\([^\/]*\)$;\1;")
+    fileName=$(echo $line | sed -e "s;.*/\([^\/]*\)$;\1;" -e "s/[^a-zA-Z0-9%-_]//g" -e "s/-//")
     if [[ -f $fileName ]]; then
         echo "$fileName already exists" > log.txt
     else
-        curl -O $line;
+        curl -o fileName $line;
     fi
 done < "tmp.txt"
